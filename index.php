508d5a424fc2152b9be755eec1e1db20a99f89b1
kadrim1 <kadri.mihhailova@khk.ee>
2015-10-06 22:14:25 +0300

    Blogipostitused esilehel dünaamiliseks [Delivers #104975878]

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
+<?
+//Connect database
+$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+
+//Retrieve data from database
+$q = mysqli_query($db, "SELECT * FROM posts NATURAL JOIN authors");
+$posts = Array();
+while ($row = mysqli_fetch_assoc($q)) {
+    $posts[] = $row;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
     <meta charset="utf-8">
@@ -93,58 +105,28 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <div class="post-preview">
-                <a href="post.html">
-                    <h2 class="post-title">
-                        Man must explore, and this is exploration at its greatest
-                    </h2>
 
-                    <h3 class="post-subtitle">
-                        Problems look mighty small from 150 miles up
-                    </h3>
-                </a>
+            <? foreach ($posts as $post): ?>
 
-                <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
-            </div>
-            <hr>
-            <div class="post-preview">
-                <a href="post.html">
-                    <h2 class="post-title">
-                        I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                    </h2>
-                </a>
+                <div class="post-preview">
 
-                <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
-            </div>
-            <hr>
-            <div class="post-preview">
-                <a href="post.html">
-                    <h2 class="post-title">
-                        Science has not yet mastered prophecy
-                    </h2>
+                    <a href="post.html">
+                        <h2 class="post-title">
+                            <?= $post['post_title'] ?>
+                        </h2>
 
-                    <h3 class="post-subtitle">
-                        We predict too much for the next year and yet far too little for the next ten.
-                    </h3>
-                </a>
+                        <h3 class="post-subtitle">
+                            <?= $post['post_description'] ?>
+                        </h3>
+                    </a>
 
-                <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</p>
-            </div>
-            <hr>
-            <div class="post-preview">
-                <a href="post.html">
-                    <h2 class="post-title">
-                        Failure is not an option
-                    </h2>
+                    <p class="post-meta">Posted by <a href="#"><?= $post['author_name'] ?></a>
+                        on <?= $post['post_created'] ?></p>
+                </div>
+                <hr>
 
-                    <h3 class="post-subtitle">
-                        Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                    </h3>
-                </a>
+            <? endforeach; ?>
 
-                <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on July 8, 2014</p>
-            </div>
-            <hr>
             <!-- Pager -->
             <ul class="pager">
                 <li class="next">
