6e7f3432cb7dfff54c94dfda6233b0b6af6b07d8
kadrim1 <kadri.mihhailova@khk.ee>
2015-10-06 22:24:28 +0300

    Pivotal+ Git ühendatud. Esialgse projektikeskkonna ülespanek [Delivers #104975856]

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ mysqli_query($db, "SET NAMES 'utf8'");
 
 //Retrieve data from database
 $q = mysqli_query($db, "SELECT * FROM posts NATURAL JOIN authors");
-
+$posts = Array();
 while ($row = mysqli_fetch_assoc($q)) {
     $posts[] = $row;
 }
