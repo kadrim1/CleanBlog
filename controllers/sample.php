e31a7dca213c3daf64832cb0de02cd0d9d3f012a
kadrim1 <kadri.mihhailova@khk.ee>
2015-10-07 23:44:25 +0300

    Sample leht [Delivers #105142840]

--- controllers/sample.php
+++ controllers/sample.php
@@ -1,8 +1,7 @@
 <?php
 
 //Retrieve random row from database
-$q = mysqli_query($db, "
-  SELECT *,DATE_FORMAT(post_created, '%d.%m.%Y %H:%i') post_created FROM posts NATURAL JOIN authors ORDER BY RAND() LIMIT 1");
+$q = mysqli_query($db, "SELECT *,DATE_FORMAT(post_created, '%d.%m.%Y %H:%i') post_created FROM posts NATURAL JOIN authors ORDER BY RAND() LIMIT 1");
 
 $post = mysqli_fetch_assoc($q);
 
