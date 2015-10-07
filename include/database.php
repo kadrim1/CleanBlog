93538380b4ae7ea3440bb965f01e3891a65dc48f
kadrim1 <kadri.mihhailova@khk.ee>
2015-10-07 23:34:43 +0300

    About leht ja View Controller süsteem lisatud [Delivers #105124188]

---
+++ include/database.php
@@ -0,0 +1,8 @@
+<?php
+//Connect database
+require_once 'config.php';
+$db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+
+
+?>
